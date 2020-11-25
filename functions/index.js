1c2325daba82fd2531e51052f1dee447e00e957c
wdvictor <victorhpantunes@gmail.com>
2020-11-25 01:14:00 -0300

    adding /issues api

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
 const functions = require("firebase-functions");
-const express = require("express");
-const commit_route = require("./commits");
+const express = require("express")
+const commit_route = require("./commits")
 const branches_routes = require('./branches')
+const issues_route = require("./issues")
 const app = express();
 
-app.get("/commits", commit_route.get);
+app.get("/commits", commit_route.get)
 
-app.get('/branches', branches_routes.get);
+app.get('/branches', branches_routes.get)
 
-exports.app = functions.https.onRequest(app);
+app.get('/issues' , issues_route.get)
+
+exports.app = functions.https.onRequest(app)
